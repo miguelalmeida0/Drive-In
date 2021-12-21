9bd010d322539345886e00122870d73549bf2a8d
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2021-12-21 15:55:37 +0000

    Uuid/Morgan instal, updated documentation file (Project 2.5)

--- index.js
+++ index.js
@@ -1,87 +1,130 @@
-//Requiring Express and Morgan
-const express = require('express');
-const morgan = require('morgan');
+  //Requiring Express and Morgan
+  const express = require('express');
+  const morgan = require('morgan');
+  const uuid = require('uuid');
 
-const app = express();
+  const app = express();
 
-app.use(morgan('common'));
+  app.use(morgan('common'));
 
-//My top 10 movie list
+  app.use(bodyParser.json());
 
-let movies = [
-{
-  title: "Harry Potter and the Philosopher\’s Stone",
-  year: "2001"
-},
+  //My top 10 movie list
 
-{
-  title: "Fight Club",
-  year: "1999"
-},
+  let movies = [
+    {
+      title: 'Harry Potter and the Philosopher\’s Stone',
+      year: '2001',
+    },
 
-{
-  title: "Parasite",
-  year: "2019"
-},
+    {
+      title: 'Fight Club',
+      year: '1999'
+    },
 
-{
-  title: "Joker",
-  year: "2019"
-},
+    {
+      title: 'Parasite',
+      year: '2019'
+    },
 
-{
-  title: "The Lord of the Rings: The Fellowship of the Ring",
-  year: "2001"
-},
+    {
+      title: 'Joker',
+      year: '2019'
+    },
 
-{
-  title: "Interstellar",
-  year: "2014"
-},
+    {
+      title: 'The Lord of the Rings: The Fellowship of the Ring',
+      year: '2001'
+    },
 
-{
-  title: "The Shawshank Redemption",
-  year: "1994"
-},
+    {
+      title: 'Interstellar',
+      year: '2014'
+    },
 
-{
-  title: "Pulp Fiction",
-  year: "1994"
-},
+    {
+      title: 'The Shawshank Redemption',
+      year: '1994'
+    },
 
-{
-  title: "Schindler\'s List",
-  year: "1993"
-},
+    {
+      title: 'Pulp Fiction',
+      year: '1994'
+    },
 
-{
-  title: "Matrix",
-  year: "1999"
-}
-];
+    {
+      title: 'Schindler\'s List',
+      year: '1993'
+    },
 
-// GET requests
-app.get("/movies", (req, res) => {
+    {
+      title: 'Matrix',
+      year: '1999'
+    }
+  ];
+
+  // GET requests
+
+  app.get('/movies', (req, res) => {
     res.json(movies);
-});
+  });
 
-app.get('/', (req, res) => {
+  app.get('/documentation', (req, res) => {
+    res.sendfile('public/documentation.html', {root: __dirname})
+  })
+
+  app.get('/', (req, res) => {
     res.send('Glad to see you in the best Drive-In!');
+  });
+
+  app.get('/movies/:title', (req, res) => {
+    res.send('Movie by title');
 });
 
-//Using Express Static to serve my documentation.html
-
-app.use(express.static("public"));
-
-//Using Error handling middleware
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong!");
+app.get('/genres/:title', (req, res) => {
+    res.send('Genre by name or title');
 });
 
-//Server is running in this port
-
-app.listen(8080, () =>{
-  console.log("This app is listening on port 8080.");
+app.get('/directors/:name', (req, res) => {
+    res.send('Info about the director by name');
 });
+
+//App post
+
+app.post('/users', (req, res) => {
+    res.send('Registration completed');
+});
+
+app.put('/users/:username', (req, res) => {
+    res.send('Information updated');
+});
+
+app.post('/users/:username/movies/:movieID', (req, res) => {
+    res.send('Movie was added to the favorite list');
+});
+
+//App Delete
+
+app.delete('/users/:username/movies/:movieID', (req, res) => {
+    res.send('Movie was deleted');
+});
+
+app.delete('/users/:username', (req, res) => {
+    res.send('Your account was sadly deleted!');
+});
+      //Using Express Static to serve my documentation.html
+
+      app.use(express.static('public'));
+
+      //Using Error handling middleware
+
+      app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send('Something went wrong!'');
+      });
+
+      //Server is running in this port
+
+      app.listen(8080, () =>{
+        console.log('This app is listening on port 8080.'');
+      });
